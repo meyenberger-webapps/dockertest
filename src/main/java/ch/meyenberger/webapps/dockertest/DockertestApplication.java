89c0d8d95f55592ae6754d1f54d74cf292c6dc64
Sven Meyenberger <development@meyenberger.ch>
2018-08-03 23:27:16 +0200

    Projektsetup

---
+++ src/main/java/ch/meyenberger/webapps/dockertest/DockertestApplication.java
@@ -0,0 +1,20 @@
+package ch.meyenberger.webapps.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockertestApplication.class, args);
+    }
+
+    @RequestMapping(value = "/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
